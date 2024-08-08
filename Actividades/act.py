662e48008034a71a97e12d703c883613ec02fd3d
Maarten van t Hoff <mvanthoff28@gmail.com>
2024-08-08 15:12:15 -0600

    Fixed issue with venv

--- Actividades/act.py
+++ Actividades/act.py
@@ -1,1 +1,14 @@
-import numpy
+import numpy as np
+
+def cuentaDias(camisas,linea1,linea2):
+    dias = 0
+    prod = 0
+    if camisas > prod:
+        prod += linea1 + linea2
+        dias += 1
+
+    elif camisas < prod:
+        dias = 1
+    return dias
+
+linea1=np.random.randint(0,10)
