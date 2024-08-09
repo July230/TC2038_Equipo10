32c91e3c1eda97971cb15e2f7a3bdfa70a4a0698
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-08-09 09:07:09 -0600

    Corregir error en ejercicio6 factorial

--- Actividades/ejercicio6.py
+++ Actividades/ejercicio6.py
@@ -8,7 +8,7 @@ def factorial(n):
     if n < 0:
         print("Factorial no existe")
         return 0
-    for i in range(1,n):
+    for i in range(1,n + 1):
         fact *= i
     print("Factorial es:", fact)
     return fact
