d70ff042ab3adf8ee687ff3827f3cdd651e33ae9
Maarten van t Hoff <mvanthoff28@gmail.com>
2024-08-09 18:18:45 -0600

    Ejercicio 1

--- Actividades/ejercicio1.py
+++ Actividades/ejercicio1.py
@@ -1,14 +1,21 @@
 import numpy as np
 
-def cuentaDias(camisas,linea1,linea2):
+def cuentaDias(camisas):
     dias = 0
-    prod = 0
-    if camisas > prod:
-        prod += linea1 + linea2
+    falta = camisas
+    while falta > 0:
+        linea1 = np.random.randint(0,9)
+        linea2 = np.random.randint(0,9)
+        maxProd = linea1 + linea2
         dias += 1
-
-    elif camisas < prod:
-        dias = 1
+        if falta > maxProd:
+            falta -= maxProd
+        elif falta > linea1 and falta > linea2:
+            falta -= max(linea1,linea2)
+        elif falta <= linea1 or falta <= linea2:
+            falta -= min(linea1,linea2)
     return dias
 
-#linea1=np.random.randint(0,10)
+N = int(input("Cuantas camisas quieres producir?\n"))
+dias = cuentaDias(N)
+print("Dias de producción:",dias)
