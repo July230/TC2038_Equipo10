3e2fcaa826f6b0d4654126539912dafc41b6108c
Maarten van t Hoff <mvanthoff28@gmail.com>
2024-08-11 21:46:18 -0600

    Arreglo de ejercicio 5

--- Actividades/ejercicio5.py
+++ Actividades/ejercicio5.py
@@ -4,12 +4,17 @@ Maarten van 't Hoff A01764070, Ian Julian Estrada Castro A01352823
 Fecha: 09/08/2024
 """
 
-# Esto es una opcion iterative en cual se usa al variable i para sumar los fracciones cuadrados
+# Esto es una opcion iterative en cual se usa al variable i para sumar los fracciones.
+# Dependiente del número sumado el numero es elevado a otro numero.
+# Si es un número de pares lo cuadramos. Si es un número de impares el exponente trae el mismo numero.
 
 def fracSquareSum(n):
     sum = 0
     for i in range(1,n):
-        sum += pow(1/i, 2)
+        if i%2 == 0:
+            sum += pow(1/i, 2)
+        else:
+            sum += pow(1/i,i)
     return sum
 
 N = 15
