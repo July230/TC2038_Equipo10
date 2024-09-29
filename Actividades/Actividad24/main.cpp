6697799f623ad57de13f8d7c88d8aeb6892ec7cc
Maarten van t Hoff <mvanthoff28@gmail.com>
2024-09-29 16:43:13 -0600

    Finished activity 24 and added test cases

--- Actividades/Actividad24/main.cpp
+++ Actividades/Actividad24/main.cpp
@@ -22,6 +22,7 @@ Fecha: 25/09/2024
 */
 
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <stdlib.h>
 #include <string>
@@ -47,64 +48,103 @@ vector<vector<char>> constructArr(string fileName, int length){
         newfile.close(); // Close file object
     }
 
+    // Remove last element of array which is newline
+    if (!tempArr.empty() && tempArr.back() == '\n'){
+        tempArr.pop_back();
+    }
+
     int numRows = (tempArr.size() + length - 1) / length;
 
     for (int i = 0; i < numRows; i++){
         vector<char> row;
         for (int j = 0; j < length; j++){
             if (i * length + j < tempArr.size()){
-                row.push_back(tempArr[i * length + j]);
+                row.push_back(tempArr[i * length + j]); // Push each element to the back of the corresponding row
             }
             else{
-                row.push_back('\0');
+                row.push_back('\0'); // If we have reached end of text array, fill the rest with null characters
             }
         }
-        arr.push_back(row);
+        arr.push_back(row); // Push row to back of the full array, reshaping the text file
     }
 
     return arr;
 }
 
-int calcHash(){
-    
+vector<int> calcHash(vector<vector<char>> &arr, int length){
+    vector<int> result;
+    int colResult = 0;
+    for (int i = 0; i < length; i++){
+        colResult = 0; // For each column, reset the result calculator
+        for (int j = 0; j < arr.size(); j++){
+            if (arr[j][i] == '\0'){
+                colResult += length; // If null character then sum user input
+            }
+            else{
+                colResult += arr[j][i]; // Sum other ASCII values
+            }
+        }
+        colResult = colResult % 256; // Calculate hash value
+        result.push_back(colResult); // Send value to back of result array
+    }
+    return result;
 }
 
 int main(){
     int n;
     string fileName;
     vector<vector<char>> str;
+    vector<int> result;
 
     cout << "Ingrese el nombre del archivo de texto (sin extension): "; getline(cin, fileName);
     ifstream file(fileName + ".txt");
-    if (!file){
-        cerr << "Archivo de texto " << fileName << ".txt no existe" << endl;
-        return -1;
+    if (!file){ // Check if the filename exists within the provided directory
+        cerr << "Archivo de texto " << fileName << ".txt no existe" << endl; // Return error text
+        return -1; // Return error code
     }
     file.close();
 
 
     cout << "Ingrese un multiplo 'n' de 4 entre 16 y 64: "; cin >> n;
-    if (n < 16 || n % 4 != 0){
-        cout << "Numero invalido" << endl;
+    if (n < 16 || n % 4 != 0){ // Check if value is valid for hash calculation
+        cerr << "Numero invalido" << endl;
         return -1;
     }
 
     str = constructArr(fileName, n);
 
-    for (const auto& row : str){
-        for (const char& c : row){
-            if (c == '\n'){
-                cout << "-" << " ";
+    // Print the values of the reshaped text array
+    cout << "Matriz generada por el algoritmo, los saltos de linea se representan con '-' y espacios rellenados con '['" << endl;
+    for (int i = 0; i < str.size(); i++){ // arr.size() takes the outermost dimension, which are the rows
+        for (int j = 0; j < n; j++){ // n provides the amount of columns
+            if (str[i][j] == '\n'){
+                cout << "-" << " "; // Fill LF characters with '-'
             }
-            else if (c == '\0'){
-                cout << "[" << " ";
+            else if (str[i][j] == '\0'){
+                cout << "[" << " "; // Fill null characters with '-'
             }
             else{
-                cout << c << " ";
+                cout << str[i][j] << " ";
             }
         }
         cout << endl;
     }
+
+    result = calcHash(str, n);
+
+    // Print the values of the calculated hash
+    cout << "Arreglo de longitud 'n' que muestra la suma de las columnas con el modulo 256" << endl;
+    for (int i = 0; i < result.size(); i++){
+        cout << result[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Representacion hexadecimal" << endl;
+    for (int i = 0; i < result.size(); i += 2){
+        cout << hex << uppercase << setfill('0') << setw(2) << result[i]; // Asures all hex values are of 2 digits using setfill and setw
+        cout << setfill('0') << setw(2) << result[i+1] << " ";
+    }
+    cout << nouppercase << dec << endl;
 
     return 0;
 }
