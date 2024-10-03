6b31fa06f728595afef0c9eaf9cce4dc42c0d92e
Maarten van t Hoff <mvanthoff28@gmail.com>
2024-10-03 10:51:15 -0600

    Started main file for AI

--- ActividadIntegradora01/Equipo_10_main/main.cpp
+++ ActividadIntegradora01/Equipo_10_main/main.cpp
@@ -1,1 +1,11 @@
 #include "header.h"
+
+int main(){
+    std::string mCodeFolder = "./mCode/";
+    std::string txtFolder = "./transmission/";
+
+    std::vector<std::string> mCode = {"mCode01.txt", "mCode02.txt", "mCode03.txt"};
+    std::vector<std::string> txt = {"transmission01.txt", "transmission02.txt"};
+    
+    return 0;
+}
