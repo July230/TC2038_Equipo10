e6546a7d69ec27356e272e8ec48a83095ca6dd40
Ian Julián Estrada Castro <A01352823@tec.mx>
2024-10-05 19:29:04 -0600

    Reducir codigo en palindromo.h

--- ActividadIntegradora01/Equipo_10_main/palindromo.h
+++ ActividadIntegradora01/Equipo_10_main/palindromo.h
@@ -1,17 +1,7 @@
-/*
-Suponiendo que el código malicioso tiene siempre código "espejeado" (palíndromos de chars), 
-sería buena idea buscar este tipo de código en una transmisión. 
-El programa busca si hay código "espejeado" dentro de los archivos de transmisión. 
-(palíndromo a nivel chars, no meterse a nivel bits). 
-El programa muestra en una sola línea dos enteros separados por un espacio correspondientes a la posición (iniciando en 1) 
-en donde inicia y termina el código "espejeado" más largo (palíndromo) para cada archivo de transmisión. 
-Puede asumirse que siempre se encontrará este tipo de código.
-*/
+#ifndef PALINDROMO
+#define PALINDROMO
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <fstream>
+#include "header.h"
 
 using namespace std;
 
@@ -71,3 +61,5 @@ pair<int, int> manacher(const string& s){
     return resultado; 
 
 }
+
+#endif
