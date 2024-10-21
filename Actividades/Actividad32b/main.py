da0c6d516e2e60876035eb51a93e5a9cc8184342
DiegoGarciaPadilla <gpdiego370@outlook.com>
2024-10-20 20:59:22 -0600

    Añadir archivo main.py con implementación de algoritmos Dijkstra y Floyd-Warshall

---
+++ Actividades/Actividad32b/main.py
@@ -0,0 +1,36 @@
+from dijkstra import *
+from floyd import *
+
+# Valor de n
+n = int(input("numero : "))
+
+# Matriz de adyacencia
+graph = [
+    [0, 2, -1, 3,],
+    [-1, 0, 1, 5],
+    [2, 3, 0, -1],
+    [3, -1, 4, 0],
+]
+
+# Dijkstra
+print("\n----- DIJKSTRA -----\n")
+
+# Inicializar grafo como diccionario para Dijkstra
+graph_dijkstra = initGraph(graph)
+
+# Llamar al algoritmo de Dijkstra
+for node in graph_dijkstra:
+    print(f"Node {node}: {graph[node]}")
+    distanceList = dijkstra(graph_dijkstra, node)
+    for dest, dist in distanceList.items():
+        if dest != node:
+            print(f"node {node} to node {dest}: {dist}")
+
+# Floyd-Warshall
+print("\n----- FLOYD-WARSHALL -----\n")
+
+# Llamar al algoritmo de Floyd-Warshall
+result = floydWarshall(n, graph)
+
+for row in result:
+    print(" ".join(map(str, row)))
