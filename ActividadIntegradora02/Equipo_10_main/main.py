ba52778dfc9ce05c05e7f83da943c76d7903e759
Ian Julián Estrada Castro <A01352823@tec.mx>
2024-11-10 19:15:56 -0600

    Adaptar FordFulkerson a los tres casos

--- ActividadIntegradora02/Equipo_10_main/main.py
+++ ActividadIntegradora02/Equipo_10_main/main.py
@@ -2,11 +2,14 @@ from readFile import readFile
 from MinSpanTree import *
 from FordFulkerson import *
 
-filename = "./inputs/E5_Entrada_2.txt"
+filename = "./inputs/E5_Entrada_1.txt"
+filename1 = "./inputs/E5_Entrada_2.txt"
 filename2 = "./inputs/E5_Entrada_3.txt"
 N, weight, flow, coords = readFile(filename)
+N1, weight1, flow1, coords1 = readFile(filename1)
 N2, weight2, flow2, coords2 = readFile(filename2)
 
+print("------------------------ Caso 1 ------------------------")
 print("N:", N)
 
 print("Weight:")
@@ -26,9 +29,27 @@ print("Construcción óptima con pesos:")
 for u, v, weight in mst:
     print(f"({u}, {v}): {weight}")
 
-# Algoritmo de Ford-Fulkerson
+# Algoritmo de Ford-Fulkerson caso 1
+initialPathNode = 0
+finalPathNode = max(flow.keys()) # Ultimo nodo del grafo
+maxFlow = fordFulkerson(flow, initialPathNode, finalPathNode)
+print("------------------------ Ford-Fulkerson ------------------------")
+print("Flujo máximo: ", maxFlow)
+
+print("------------------------ Caso 2 ------------------------")
+
+# Algoritmo de Ford-Fulkerson caso 2
+initialPathNode = 0
+finalPathNode = max(flow1.keys()) # Ultimo nodo del grafo
+maxFlow1 = fordFulkerson(flow1, initialPathNode, finalPathNode)
+print("------------------------ Ford-Fulkerson ------------------------")
+print("Flujo máximo: ", maxFlow1)
+
+print("------------------------ Caso 3 ------------------------")
+
+# Algoritmo de Ford-Fulkerson caso 3
 initialPathNode = 0
 finalPathNode = max(flow2.keys()) # Ultimo nodo del grafo
-maxFlow = fordFulkerson(flow2, initialPathNode, finalPathNode)
-print("------------------------ Ford-Fulkerson caso 3 ------------------------")
-print("Flujo máximo: ", maxFlow)
+maxFlow2 = fordFulkerson(flow2, initialPathNode, finalPathNode)
+print("------------------------ Ford-Fulkerson ------------------------")
+print("Flujo máximo: ", maxFlow2)
